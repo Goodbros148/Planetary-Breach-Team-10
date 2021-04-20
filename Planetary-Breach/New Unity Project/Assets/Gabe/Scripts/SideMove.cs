9abc3e6e745c8398b8567715a3f82d130e1c638b
Goodbros148 <1551850@mukilteo.wednet.edu>
2021-04-20 13:49:27 -0700

    Added working health system and health bar. Placeholder healthbar

--- Planetary-Breach/New Unity Project/Assets/Gabe/Scripts/SideMove.cs
+++ Planetary-Breach/New Unity Project/Assets/Gabe/Scripts/SideMove.cs
@@ -7,12 +7,16 @@ public class SideMove : MonoBehaviour
     public float accel = 8;
     private Rigidbody2D rb2;
     private SpriteRenderer sr;
+    Vector3 characterScale;
+    float characterScaleX;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2 = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        characterScale = transform.localScale;
+        characterScaleX = characterScale.x;
     }
 
     private void FixedUpdate()
@@ -21,16 +25,27 @@ public class SideMove : MonoBehaviour
         //Move right
         if(Input.GetAxis("Horizontal")>0)
         {
-            sr.flipX = false;
             rb2.AddForce(new Vector2(accel, 0));
         }
 
         //Move Left
         if (Input.GetAxis("Horizontal") < 0)
         {
-            sr.flipX = true;
             rb2.AddForce(new Vector2(-accel, 0));
         }
 
+    }
+    //Flip sprite (still wip)
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -characterScaleX;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = characterScaleX;
+        }
+        transform.localScale = characterScale;
     }
 }
