eab467417f7d8828ed8c450a19bd0a49623ad3b8
Goodbros148 <1551850@mukilteo.wednet.edu>
2021-04-21 18:54:47 -0700

    Revert "Hazards can cause damage. Need invincibility frame to be coded still"
    This reverts commit 89541044d797f018eebba393d51c22f451fbc141.

--- Planetary-Breach/New Unity Project/Assets/Gabe/Scripts/Health.cs
+++ Planetary-Breach/New Unity Project/Assets/Gabe/Scripts/Health.cs
@@ -1,15 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    public static int curHealth = 0;
+    public int curHealth = 0;
     public int maxHealth = 50;
     public HealthBar healthBar;
-    private bool isInvincible = false;
-    private float invincibilityDurSec;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +14,8 @@ public class Health : MonoBehaviour
         curHealth = maxHealth;
     }
 
-
     // Update is called once per frame
+    //void Update()
     //For debug purposes.
     //{
     //    if (Input.GetKeyDown(KeyCode.Space))
@@ -29,16 +26,7 @@ public class Health : MonoBehaviour
 
     public void DamagePlayer(int damage)
     {
-        if (isInvincible) return;
-
         curHealth -= damage;
         healthBar.SetHealth(curHealth);
-        //death
-        if (curHealth < 1)
-        {
-            SceneManager.LoadScene("Death");
-            return;
-        }
     }
-
 }
