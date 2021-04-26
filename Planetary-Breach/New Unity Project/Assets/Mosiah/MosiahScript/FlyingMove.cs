56f97b9f90395ce89af3d2b28c2cfa98e599de0b
MosiahF <17077225@mukilteo.wednet.edu>
2021-04-26 13:08:24 -0700

    Started Enemy Testing in Mosiah Folder

---
+++ Planetary-Breach/New Unity Project/Assets/Mosiah/MosiahScript/FlyingMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingMove : MonoBehaviour
+{
+    public Rigidbody2D rb2a;
+    public Rigidbody2D rb2b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Planetary-Breach/New Unity Project/Assets/Mosiah/MosiahScript/Health2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health2 : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 50;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //For debug purposes.
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        DamagePlayer(10);
+    //    }
+    //}
+
+    public void DamagePlayer(int damage)
+    {
+        curHealth -= damage;
+        healthBar.SetHealth(curHealth);
+    }
+}
