81d97cff96b076a41c694ef0b3493407d71bf59e
Leo <leo_1517@outlook.com>
2021-04-26 13:24:24 -0700

    Progress on shooting mechanic

---
+++ Planetary-Breach/New Unity Project/Assets/Leo/Scripts/BulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("DestroySelf", 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(10, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            DestroySelf();
+        }
+    }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
+
---
+++ Planetary-Breach/New Unity Project/Assets/Leo/Scripts/ShootingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    Object bulletRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRef = Resources.Load("Bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject Bullet = (GameObject)Instantiate(bulletRef);
+            Bullet.transform.position = new Vector3(transform.position.x + 0.6f, transform.position.y + 0.2f, -1);
+        }
+    }
+}
