6fcf2430e1ffaf12e15dfad18dfadb4f7691a026
MosiahF <17077225@mukilteo.wednet.edu>
2021-04-28 20:30:44 -0700

    Enemy Work, Enemy follows and has a follow point which it stays around.

---
+++ Planetary-Breach/New Unity Project/Assets/Mosiah/MosiahScript/FlyEnemyMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyEnemyMove : MonoBehaviour
+{
+    private SideMove2 thePlayer;
+    public GameObject playerObject;
+    public GameObject followPoint;
+    public float moveSpeed;
+
+    public float playerRange;
+
+    public LayerMask playerLayer;
+
+    public bool playerInRange;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<SideMove2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInRange = Physics2D.OverlapCircle(transform.position, playerRange, playerLayer);
+
+        if (playerInRange)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, playerRange);
+    }
+}
